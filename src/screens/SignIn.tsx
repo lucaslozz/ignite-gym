10df063b7542cafd22637b738d646dca17e085f4
lucaslozz <lucass.mark@gmail.com>
2023-05-08 09:07:29 -0300

    add component input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,8 @@
-import { VStack, Image, Text, Center } from "native-base"
+import { VStack, Image, Text, Center, Heading } from "native-base"
 
 import BackgroundImg from "../assets/background.png"
 import LogoSvg from "../assets/logo.svg"
+import { Input } from "../components/Input"
 
 
 export function SignIn() {
@@ -14,6 +15,14 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily={"heading"}>
+          Acesse sua conta
+        </Heading>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   )
