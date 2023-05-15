27bfa60157a0c36d9a394523fc3073acf6fe502a
lucaslozz <lucass.mark@gmail.com>
2023-05-15 16:04:12 -0300

    add HomeHeader component

---
+++ src/components/UserPhoto.tsx
@@ -0,0 +1,17 @@
+import { Image, IImageProps } from "native-base"
+
+interface UserPhotoProps extends IImageProps {
+  size: number
+}
+
+export function UserPhoto({ size, ...props }: UserPhotoProps) {
+  return (
+    <Image
+      w={size}
+      h={size}
+      rounded="full"
+      borderWidth={2}
+      borderColor="gray.400"
+      {...props} />
+  )
+}
