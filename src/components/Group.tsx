1d16f90ae8202febeb8b822f4cd7a3701f476786
lucaslozz <lucass.mark@gmail.com>
2023-05-16 10:10:54 -0300

    add exercise list

---
+++ src/components/Group.tsx
@@ -0,0 +1,37 @@
+import { Pressable, Text, IPressableProps } from 'native-base';
+import { border } from 'native-base/lib/typescript/theme/styled-system';
+
+interface Group extends IPressableProps {
+  name: string;
+  isActive: boolean;
+}
+
+export function Group({ name, isActive, ...props }: Group) {
+  return (
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: 'green.500',
+        borderWidth: 1,
+      }}
+      {...props}
+    >
+      <Text
+        color={isActive ? 'green.500' : 'gray.200'}
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
+    </Pressable>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,36 @@
-import { VStack } from "native-base";
-import { HomeHeader } from "../components/HomeHeader";
+import { FlatList, HStack, VStack } from 'native-base';
+import { HomeHeader } from '../components/HomeHeader';
+import { Group } from '../components/Group';
+import { useState } from 'react';
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    'costas',
+    'bícipes',
+    'Trícepes',
+    'ombro',
+  ]);
+  const [groupSelected, setGroupSelected] = useState('costas');
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
-  )
+  );
 }
