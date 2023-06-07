9a4df5d82b037410d35a2826b14eecb6d82c0e94
lucaslozz <lucass.mark@gmail.com>
2023-06-07 09:39:01 -0300

    loades user logged

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext, useEffect, useState } from 'react';
 
 import { api } from '../services/api';
 import { UserDTO } from '../dtos/UserDTO';
+import { storageUserGet, storageUserSave } from '../storage/storageUser';
 
 export interface AuthContextDataProps {
   user: UserDTO;
@@ -25,11 +26,25 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
     }
   }
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet();
+
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
